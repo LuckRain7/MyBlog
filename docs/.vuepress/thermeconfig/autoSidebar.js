10842bb724b58cdec4f7b70342b2de800a7c5b4b
LuckRain <763839938@qq.com>
2020-03-21 23:25:54 +0800

    'update'

--- docs/.vuepress/thermeconfig/autoSidebar.js
+++ docs/.vuepress/thermeconfig/autoSidebar.js
@@ -1,1 +1,1 @@
-module.exports = {"/share/":["css","JavaScript","Node","webpacka","前端工程化","前端框架","玩转GitHub","笔记","跨端"],"/tool/":["Git","vscode插件推荐","包管理器","网站推荐","软件推荐","配置文件"],"/WebGIS/":["ArcGIS","Cesium","OpenLayer",""]}
+module.exports = {"/share/":["css","JavaScript","Node","webpacka","前端工程化","前端框架","玩转GitHub","笔记","跨端"],"/tool/":["Git","vscode必备","包管理器","网站推荐","软件推荐","配置文件"],"/WebGIS/":["ArcGIS","Cesium","OpenLayer",""]}
