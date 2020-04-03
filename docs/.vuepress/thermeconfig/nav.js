ec90c09734a1b24bbfb4d691c835779db13d12ec
LuckRain <763839938@qq.com>
2020-04-03 22:39:35 +0800

    更新 nav

--- docs/.vuepress/thermeconfig/nav.js
+++ docs/.vuepress/thermeconfig/nav.js
@@ -1,4 +1,9 @@
 /*
+ *  Description:
+ *  Author: LuckRain7
+ *  Date: 2020-03-15 17:40:55
+ */
+/*
  * 导航栏设置
  */
 module.exports = [
@@ -12,36 +17,36 @@ module.exports = [
       { text: "TypeScript", link: "/frontend/TypeScript/" },
       { text: "Vue", link: "/Vue/Vue/" },
       { text: "React", link: "/React/React/" },
-      { text: "Webpack", link: "/frontend/Webpack/" }
-    ]
+      { text: "Webpack", link: "/frontend/Webpack/" },
+    ],
   },
   {
     text: "前端工程化",
-    link: "/engineering/engineering/"
+    link: "/engineering/engineering/",
   },
   {
     text: "服务端",
     items: [
       { text: "Node.js", link: "/server/Node/" },
       { text: "Golang", link: "/server/Golang/" },
-      { text: "Python", link: "/server/Python/" }
-    ]
+      { text: "Python", link: "/server/Python/" },
+    ],
   },
   {
     text: "WebGIS",
     items: [
       { text: "Cesium", link: "/WebGIS/Cesium/" },
       { text: "ArcGIS", link: "/WebGIS/ArcGIS/" },
-      { text: "OpenLayer", link: "/WebGIS/OpenLayer/" }
-    ]
+      { text: "OpenLayer", link: "/WebGIS/OpenLayer/" },
+    ],
   },
   {
     text: "必备工具",
     items: [
       { text: "包管理器", link: "/tools/package/" },
       { text: "Git", link: "/tools/Git/" },
-      { text: "GitHub", link: "/tools/GitHub/" }
-    ]
+      { text: "GitHub", link: "/tools/GitHub/" },
+    ],
   },
   {
     text: "个人分享",
@@ -49,8 +54,9 @@ module.exports = [
       { text: "VS Code必备", link: "/share/vscode/" },
       { text: "软件推荐", link: "/share/software/" },
       { text: "网站推荐", link: "/share/website/" },
+      { text: "前端工具框架推荐", link: "/frame/index.html" },
       { text: "配置文件", link: "/share/config/" },
-      { text: "笔记", link: "/share/notes/" }
-    ]
-  }
+      { text: "笔记", link: "/share/notes/" },
+    ],
+  },
 ];
