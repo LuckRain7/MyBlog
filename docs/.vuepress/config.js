9f69647efb37cc02d2e08f8e3508b2e620caf1bb
LuckRain <763839938@qq.com>
2020-04-24 20:06:46 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,8 @@
  *  Date: 2020-03-15 17:40:55
  */
 module.exports = {
-  title: "震雨Rain",
-  description: "震雨rain的博客,Webgis,arcgis,openlayer,css,javascript",
+  title: "震雨LuckRain7",
+  description: "震雨LuckRain7的博客,Webgis,arcgis,openlayer,css,javascript",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   markdown: {
     // lineNumbers: true // 代码块显示行号
@@ -17,7 +17,7 @@ module.exports = {
   },
   themeConfig: {
     //导航栏 logo
-    logo: "http://img.rain7.top/rain7.top.logo.png",
+    logo: "https://rain7.top/luckrain7.png",
     // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     sidebarDepth: 1,
     // 文档更新时间：每个文件git最后提交的时间
